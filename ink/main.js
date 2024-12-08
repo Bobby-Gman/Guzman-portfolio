bf0a62cfab74f5ae39324a37b567e214fd1efb9a
RobbyGman <ro305529@ucf.edu>
2024-12-08 02:55:33 -0500

    renaming files

--- ink/main.js
+++ ink/main.js
@@ -1,6 +1,5 @@
 (function(storyContent) {
 
-    // Create ink story from the content using inkjs
     var story = new inkjs.Story(storyContent);
 
     var savePoint = "";
@@ -8,22 +7,16 @@
     let savedTheme;
     let globalTagTheme;
 
-    // Global tags - those at the top of the ink file
-    // We support:
-    //  # theme: dark
-    //  # author: Your Name
     var globalTags = story.globalTags;
     if( globalTags ) {
         for(var i=0; i<story.globalTags.length; i++) {
             var globalTag = story.globalTags[i];
             var splitTag = splitPropertyTag(globalTag);
 
-            // THEME: dark
             if( splitTag && splitTag.property == "theme" ) {
                 globalTagTheme = splitTag.val;
             }
 
-            // author: Your Name
             else if( splitTag && splitTag.property == "author" ) {
                 var byline = document.querySelector('.byline');
                 byline.innerHTML = "by "+splitTag.val;
@@ -34,44 +27,32 @@
     var storyContainer = document.querySelector('#story');
     var outerScrollContainer = document.querySelector('.outerContainer');
 
-    // page features setup
     setupTheme(globalTagTheme);
     var hasSave = loadSavePoint();
     setupButtons(hasSave);
 
-    // Set initial save point
     savePoint = story.state.toJson();
 
-    // Kick off the start of the story!
     continueStory(true);
 
-    // Main story processing function. Each time this is called it generates
-    // all the next content up as far as the next set of choices.
     function continueStory(firstTime) {
 
         var paragraphIndex = 0;
         var delay = 0.0;
 
-        // Don't over-scroll past new content
         var previousBottomEdge = firstTime ? 0 : contentBottomEdgeY();
 
-        // Generate story text - loop through available content
         while(story.canContinue) {
 
-            // Get ink to generate the next paragraph
             var paragraphText = story.Continue();
             var tags = story.currentTags;
 
-            // Any special tags included with this line
             var customClasses = [];
             for(var i=0; i<tags.length; i++) {
                 var tag = tags[i];
 
-                // Detect tags of the form "X: Y". Currently used for IMAGE and CLASS but could be
-                // customised to be used for other things too.
                 var splitTag = splitPropertyTag(tag);
 
-                // AUDIO: src
                 if( splitTag && splitTag.property == "AUDIO" ) {
                   if('audio' in this) {
                     this.audio.pause();
@@ -82,7 +63,6 @@
                   this.audio.play();
                 }
 
-                // AUDIOLOOP: src
                 else if( splitTag && splitTag.property == "AUDIOLOOP" ) {
                   if('audioLoop' in this) {
                     this.audioLoop.pause();
@@ -94,7 +74,6 @@
                   this.audioLoop.loop = true;
                 }
 
-                // IMAGE: src
                 if( splitTag && splitTag.property == "IMAGE" ) {
                     var imageElement = document.createElement('img');
                     imageElement.src = splitTag.val;
@@ -104,33 +83,26 @@
                     delay += 200.0;
                 }
 
-                // LINK: url
                 else if( splitTag && splitTag.property == "LINK" ) {
                     window.location.href = splitTag.val;
                 }
 
-                // LINKOPEN: url
                 else if( splitTag && splitTag.property == "LINKOPEN" ) {
                     window.open(splitTag.val);
                 }
 
-                // BACKGROUND: src
                 else if( splitTag && splitTag.property == "BACKGROUND" ) {
                     outerScrollContainer.style.backgroundImage = 'url('+splitTag.val+')';
                 }
 
-                // CLASS: className
                 else if( splitTag && splitTag.property == "CLASS" ) {
                     customClasses.push(splitTag.val);
                 }
 
-                // CLEAR - removes all existing content.
-                // RESTART - clears everything and restarts the story from the beginning
                 else if( tag == "CLEAR" || tag == "RESTART" ) {
                     removeAll("p");
                     removeAll("img");
 
-                    // Comment out this line if you want to leave the header visible when clearing
                     setVisible(".header", false);
 
                     if( tag == "RESTART" ) {
@@ -140,57 +112,42 @@
                 }
             }
 
-            // Create paragraph element (initially hidden)
             var paragraphElement = document.createElement('p');
             paragraphElement.innerHTML = paragraphText;
             storyContainer.appendChild(paragraphElement);
 
-            // Add any custom classes derived from ink tags
             for(var i=0; i<customClasses.length; i++)
                 paragraphElement.classList.add(customClasses[i]);
 
-            // Fade in paragraph after a short delay
             showAfter(delay, paragraphElement);
             delay += 200.0;
         }
 
-        // Create HTML choices from ink choices
         story.currentChoices.forEach(function(choice) {
 
-            // Create paragraph with anchor element
             var choiceParagraphElement = document.createElement('p');
             choiceParagraphElement.classList.add("choice");
             choiceParagraphElement.innerHTML = `<a href='#'>${choice.text}</a>`
             storyContainer.appendChild(choiceParagraphElement);
 
-            // Fade choice in after a short delay
             showAfter(delay, choiceParagraphElement);
             delay += 200.0;
 
-            // Click on choice
             var choiceAnchorEl = choiceParagraphElement.querySelectorAll("a")[0];
             choiceAnchorEl.addEventListener("click", function(event) {
 
-                // Don't follow <a> link
                 event.preventDefault();
 
-                // Remove all existing choices
                 removeAll(".choice");
 
-                // Tell the story where to go next
                 story.ChooseChoiceIndex(choice.index);
 
-                // This is where the save button will save from
                 savePoint = story.state.toJson();
 
-                // Aaand loop
                 continueStory();
             });
         });
 
-        // Extend height to fit
-        // We do this manually so that removing elements and creating new ones doesn't
-        // cause the height (and therefore scroll) to jump backwards temporarily.
         storyContainer.style.height = contentBottomEdgeY()+"px";
 
         if( !firstTime )
@@ -203,7 +160,6 @@
 
         setVisible(".header", true);
 
-        // set save point to here
         savePoint = story.state.toJson();
 
         continueStory(true);
@@ -211,24 +167,15 @@
         outerScrollContainer.scrollTo(0, 0);
     }
 
-    // -----------------------------------
-    // Various Helper functions
-    // -----------------------------------
-
-    // Fades in an element after a specified delay
     function showAfter(delay, el) {
         el.classList.add("hide");
         setTimeout(function() { el.classList.remove("hide") }, delay);
     }
 
-    // Scrolls the page down, but no further than the bottom edge of what you could
-    // see previously, so it doesn't go too far.
     function scrollDown(previousBottomEdge) {
 
-        // Line up top of screen with the bottom of where the previous content ended
         var target = previousBottomEdge;
 
-        // Can't go further than the very bottom of the page
         var limit = outerScrollContainer.scrollHeight - outerScrollContainer.clientHeight;
         if( target > limit ) target = limit;
 
@@ -240,22 +187,18 @@
         function step(time) {
             if( startTime == null ) startTime = time;
             var t = (time-startTime) / duration;
-            var lerp = 3*t*t - 2*t*t*t; // ease in/out
+            var lerp = 3*t*t - 2*t*t*t;
             outerScrollContainer.scrollTo(0, (1.0-lerp)*start + lerp*target);
             if( t < 1 ) requestAnimationFrame(step);
         }
         requestAnimationFrame(step);
     }
 
-    // The Y coordinate of the bottom end of all the story content, used
-    // for growing the container, and deciding how far to scroll.
     function contentBottomEdgeY() {
         var bottomElement = storyContainer.lastElementChild;
         return bottomElement ? bottomElement.offsetTop + bottomElement.offsetHeight : 0;
     }
 
-    // Remove all elements that match the given selector. Used for removing choices after
-    // you've picked one, as well as for the CLEAR and RESTART tags.
     function removeAll(selector)
     {
         var allElements = storyContainer.querySelectorAll(selector);
@@ -265,7 +208,6 @@
         }
     }
 
-    // Used for hiding and showing the header when you CLEAR or RESTART the story respectively.
     function setVisible(selector, visible)
     {
         var allElements = storyContainer.querySelectorAll(selector);
@@ -278,9 +220,6 @@
         }
     }
 
-    // Helper for parsing out tags of the form:
-    //  # PROPERTY: value
-    // e.g. IMAGE: source path
     function splitPropertyTag(tag) {
         var propertySplitIdx = tag.indexOf(":");
         if( propertySplitIdx != null ) {
@@ -295,7 +234,6 @@
         return null;
     }
 
-    // Loads save state if exists in the browser memory
     function loadSavePoint() {
 
         try {
@@ -310,10 +248,8 @@
         return false;
     }
 
-    // Detects which theme (light or dark) to use
     function setupTheme(globalTagTheme) {
 
-        // load theme from browser memory
         var savedTheme;
         try {
             savedTheme = window.localStorage.getItem('theme');
@@ -321,7 +257,6 @@
             console.debug("Couldn't load saved theme");
         }
 
-        // Check whether the OS/browser is configured for dark mode
         var browserDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
         if (savedTheme === "dark"
@@ -330,7 +265,6 @@
             document.body.classList.add("dark");
     }
 
-    // Used to hook up the functionality for global functionality buttons
     function setupButtons(hasSave) {
 
         let rewindEl = document.getElementById("rewind");
